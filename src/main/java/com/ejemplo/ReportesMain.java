78c869a3a1e0d4486901596490b567a90f174f52
DeividN21 <145998414+DeividN21@users.noreply.github.com>
2025-07-08 00:44:33 +0000

    Cambio

--- src/main/java/com/ejemplo/ReportesMain.java
+++ src/main/java/com/ejemplo/ReportesMain.java
@@ -21,7 +21,8 @@ public final class ReportesMain {
    */
   public static void main(final String[] args) {
     try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
-      w.println("<h1>Reportes de Análisis de Código:</h1>");
+      w.println("<h1>Reportes de Análisis de Código</h1>");
+      w.println("<h2>Lista de reportes:</h2>");
       w.println("<p><a href=\"checkstyle.html\">Ver reporte Checkstyle</a></p>");
       w.println("<p><a href=\"pmd.html\">Ver reporte PMD</a></p>");
       w.println("<p><a href=\"jacoco/index.html\">Ver reporte JaCoCo</a></p>");
